a26e1389eb4331352d164c875911f6a82800418b
SuminDV <sumin.dmytro@gmail.com>
2019-12-03 17:03:08 +0200

    Updates

---
+++ precache-manifest.c2788aaf32f194fc128fa1c8cdc5962a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f018d296a085eb237d015942470afe",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "3e72e0cfa3b2b3cecc93",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "e7c5eab61da0ca75621b",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.c87274be.chunk.css"
+  },
+  {
+    "revision": "3e72e0cfa3b2b3cecc93",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.ff6331df.chunk.js"
+  },
+  {
+    "revision": "e7c5eab61da0ca75621b",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.453268f7.chunk.js"
+  },
+  {
+    "revision": "2e720d2413893c34c645",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.36ba4591.js"
+  }
+]);
